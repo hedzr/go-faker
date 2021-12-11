4b76a5921b15cfa964f109b990ee7a7780f5117a
hz <hedzrz@gmail.com>
2021-12-11 19:08:53 +0800

    bump to v0.2.4 - A temporary release

--- cli/app/doc.go
+++ cli/app/doc.go
@@ -11,7 +11,7 @@ const (
 	// AppDesc const
 	AppDesc = "faker is an faked records generator. It makes an demo application for `cmdr`"
 	// Version const
-	Version = "0.2.5"
+	Version = "0.2.4"
 	// VersionInt const
-	VersionInt = 0x000205
+	VersionInt = 0x000204
 )
