5300d3d9edb51850c86672c523cff038b0641ff2
hz <hedzrz@gmail.com>
2023-02-16 22:20:36 +0800

    bump to v0.3.1
    - upgrade with new cmdr
    - fix ci script
    
    Signed-off-by: hz <hedzrz@gmail.com>

--- cli/app/doc.go
+++ cli/app/doc.go
@@ -11,7 +11,7 @@ const (
 	// AppDesc const
 	AppDesc = "faker is an faked records generator. It makes an demo application for `cmdr`"
 	// Version const
-	Version = "0.2.7"
+	Version = "0.3.1"
 	// VersionInt const
-	VersionInt = 0x000207
+	VersionInt = 0x000301
 )
